902fb22c8c403c366cb225b3795242d1bc435290
Saravanakumar <Saravanakumar@Saravana>
2017-06-22 23:55:11 +0530

    un common element in array

--- src/oops/Ex1.java
+++ src/oops/Ex1.java
@@ -1,5 +1,56 @@
 package oops;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.ArrayList;
+
 public class Ex1 {
+	
+	public static void main(String...args) {
+		//getting a uncommon element in a array 
+		
+		/*Integer a[]={1,2,3,4,5};
+		Integer b[]={3,4,5,6,7,8,9};
+		
+		Set<Integer> set1 =new HashSet<Integer>(Arrays.asList(a));
+		Set<Integer> set2 =new HashSet<Integer>(Arrays.asList(b));
+		
+		Set<Integer> result =new HashSet<Integer>(set1);
+		Set<Integer> temp =new HashSet<Integer>(set2);
+		
+	  
+		result.removeAll(set2);
+		temp.removeAll(set1);
+		result.addAll(temp);
+		
+		Iterator<Integer> itr=result.iterator();
+		while(itr.hasNext()){
+		    System.out.println("2-"+itr.next());//exe result 126789
+		}*/
+		
+		
+		Integer a[]={1,2,3,4,5};
+		Integer b[]={3,4,5,6,7,8,9};
+		
+		Set<Integer> set1 =new HashSet<Integer>(Arrays.asList(a));
+		Set<Integer> set2 =new HashSet<Integer>(Arrays.asList(b));
+		Set<Integer> temp =new HashSet<Integer>(set1);
+	
+		set1.removeAll(set2);
+		System.out.println(set1);
+		temp.retainAll(set2);
+		System.out.println(temp);
+		set2.removeAll(temp);
+		System.out.println(set2);
+		set1.addAll(set2);
+		System.out.println(set1);
+
+		
+	}
 
 }
