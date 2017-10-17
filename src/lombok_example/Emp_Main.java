e66170d2e95b461c9334f561bfb425377e7804ac
saravanakumar <saravanakumar@.ncssoft.in>
2017-10-17 17:28:40 +0530

    LOMBOK Examples

---
+++ src/lombok_example/Emp_Main.java
@@ -0,0 +1,9 @@
+package lombok_example;
+import lombok.Cleanup;
+public class Emp_Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	  Employee emp=new Employee();
+	//  emp.setEmpNo("EMP1");
+	}
+}
---
+++ src/lombok_example/Employee.java
@@ -0,0 +1,8 @@
+package lombok_example;
+import lombok.Data;
+@Data
+public class Employee {
+	private String empNo;
+	private String empName;
+	private String empAddress;
+}
